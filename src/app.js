3dbb8b2d2cbb295ff01935ea20bfe7b0fe0b6d2d
Lucas Reis <luk.wik40@gmail.com>
2023-06-04 14:30:36 -0300

    Conectando aplicação com banco de dados mongoDB

--- src/app.js
+++ src/app.js
@@ -1,26 +1,36 @@
 import express from "express";
+import db from "./config/dbConnect.js";
+import livros from "./models/livro.js";
+
+db.on("error", console.log.bind(console, "Erro de conexão"));
+
+db.once("open", () => {
+  console.log("Conexão com o banco feita com sucesso");
+});
 
 const app = express();
 
 app.use(express.json());
 
-const livros = [
-  {
-    id: 1,
-    titulo: "Senhor dos Aneis",
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit",
-  },
-];
+// const livros = [
+//   {
+//     id: 1,
+//     titulo: "Senhor dos Aneis",
+//   },
+//   {
+//     id: 2,
+//     titulo: "O Hobbit",
+//   },
+// ];
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node");
 });
 
 app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+  livros.find((error, livros) => {
+    res.status(200).json(livros);
+  });
 });
 
 app.get("/livros/:id", (req, res) => {
---
+++ src/config/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://lucao:lucao@alura.y5g4ji9.mongodb.net/alura-node?"
+);
+
+let db = mongoose.connection;
+
+export default db;
